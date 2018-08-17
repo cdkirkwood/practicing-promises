baeeea20fbd36e899292d6de93ced6144592434b
Connor Kirkwood <cdkirkwood@gmail.com>
2018-08-17 13:46:46 -0400

    finished chapter 1

--- src/pledge.js
+++ src/pledge.js
@@ -4,20 +4,31 @@ Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+function $Promise(executor) {
+  if (typeof executor !== 'function') throw new TypeError('executor is not a function')
+
+  this._state = 'pending'
+
+  const resolve = this._internalResolve.bind(this)
+  const reject = this._internalReject.bind(this)
+  executor(resolve, reject)
+}
+
+$Promise.prototype._internalResolve = function (data) {
+  if (this._state === 'pending') {
+    this._state = 'fulfilled'
+    this._value = data
+  }
+}
 
 
+$Promise.prototype._internalReject = function (err) {
+  if (this._state === 'pending') {
+    this._state = 'rejected'
+    this._value = err
+  }
+}
 
-
-
-/*-------------------------------------------------------
-The spec was designed to work with Test'Em, so we don't
-actually use module.exports. But here it is for reference:
-
-module.exports = $Promise;
-
-So in a Node-based project we could write things like this:
-
-var Promise = require('pledge');
-…
-var promise = new Promise(function (resolve, reject) { … });
---------------------------------------------------------*/
+$Promise.prototype.resolve = function (data) {
+  this._internalResolve(data)
+}
